4ee381e80af07ed83686dbdcade28d844bff66f9
Aleksey Belov <belovnow@gmail.com>
2024-01-31 21:16:49 +0300

    Убрал лишние скрипты

--- src/script.js
+++ src/script.js
@@ -24,34 +24,10 @@ function handleImageHover() {
     img.onmouseenter = () => img.src = "public/images/photo.jpg";
     img.onmouseleave = () => img.src = "public/images/eralash.jpg";
   }
-
 }
 
 function isTouchDevice() {
   return ('ontouchstart' in window) ||
     (navigator.maxTouchPoints > 0) ||
     (navigator.msMaxTouchPoints > 0);
-}
-
-const button = document.getElementById("notifications");
-button.addEventListener("click", () => {
-  Notification.requestPermission().then((result) => {
-    if (result === "granted") {
-      randomNotification();
-    }
-  });
-});
-
-const buttonVibro = document.getElementById("vibro");
-buttonVibro.addEventListener("click", () => navigator.vibrate(200));
-
-function randomNotification() {
-  const notifTitle = "Тест титл";
-  const notifBody = "Тест бади";
-  const notifImg = "public/icons/pwa_icon_144x144.png";
-  const options = {
-    body: notifBody,
-    icon: notifImg,
-  };
-  new Notification(notifTitle, options);
 }
